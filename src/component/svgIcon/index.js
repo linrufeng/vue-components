b127cc99670af481520a7202f22ae49443d05694
lilinsen <lilinsen@jd.com>
2019-10-18 16:18:16 +0800

    svg雪碧图

---
+++ src/component/svgIcon/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import SvgIcon from './svgIcon.vue'
+Vue.component('svg-icon', SvgIcon);
+const req = require.context('./../../asset/svgSprite', false, /\.svg$/)
+const requireAll = requireContext => {
+    requireContext.keys().map(requireContext)
+}
+
+
+requireAll(req)
+// 使用 import './components/svgIcon'
